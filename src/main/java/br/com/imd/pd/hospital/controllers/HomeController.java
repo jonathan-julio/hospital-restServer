047eb34cfe0c8836ffba6e381e1abe5cee339634
jonathan-julio <jonathan.goncalves.703@ufrn.edu.br>
2023-05-09 23:29:14 -0300

    fix bug cordenadas em HomeController

--- src/main/java/br/com/imd/pd/hospital/controllers/HomeController.java
+++ src/main/java/br/com/imd/pd/hospital/controllers/HomeController.java
@@ -1,35 +1,29 @@
 package br.com.imd.pd.hospital.controllers;
 
-import java.rmi.RemoteException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.json.JSONObject;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import br.com.imd.pd.hospital.models.Hospital;
+import br.com.imd.pd.hospital.models.Location;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-    private final RestTemplate restTemplate = new RestTemplate();
     @GetMapping
-    public String home(){
+    public String home() {
         return "home";
     }
 
@@ -37,36 +31,35 @@ public class HomeController {
     @RequestMapping("/resultado")
     public String resultado(@RequestParam("latitude") String latitude, 
                          @RequestParam("longitude") String longitude, 
-                         Model model) throws JsonMappingException, JsonProcessingException, RemoteException {
+                         Model model) throws IOException {
 
-         RestTemplate restTemplate = new RestTemplate();
-         HttpHeaders headers = new HttpHeaders();
-         headers.setContentType(MediaType.APPLICATION_JSON);
-     
-         JSONObject locationJson = new JSONObject();
-         locationJson.put("latitude", latitude);
-         locationJson.put("longitude", longitude);
-     
-         JSONObject requestJson = new JSONObject();
-         requestJson.put("name", "Hospital Bahia");
-         requestJson.put("vacancies", 1);
-         requestJson.put("location", locationJson);
-         requestJson.put("avaliable", true);
-     
-         HttpEntity<String> request = new HttpEntity<>(requestJson.toString(), headers);
-     
-         String url = "http://localhost:8080/server/location";
-         ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-         ObjectMapper mapper = new ObjectMapper();
-            mapper.registerSubtypes(HospitalImpl.class);
-            Hospital hospital = mapper.readValue(response.getBody(), HospitalImpl.class);
+        String url = "http://localhost:8080/server/location";
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
 
-         model.addAttribute("name", hospital.getName() );
-         model.addAttribute("vacancies", hospital.getVacancies() );
-         model.addAttribute("latitude", hospital.getLocation().getLatitude() );
-         model.addAttribute("longitude", hospital.getLocation().getLongitude());
-     
-         return "resultado";
+        String jsonBody = "{ \"latitude\": "+latitude+ ", \"longitude\": "+longitude+ " }";
+
+        String response = HttpUtils.post(url, headers, jsonBody);
+
+        JsonElement jsonElement = JsonParser.parseString(response);
+        JsonObject object = jsonElement.getAsJsonObject();
+
+        String name = object.get("name").getAsString();
+        int vacancies = object.get("vacancies").getAsInt();
+        JsonElement locationElement = object.get("location");
+
+        double responseLat = locationElement.getAsJsonObject().get("latitude").getAsDouble();
+        double responseLon = locationElement.getAsJsonObject().get("longitude").getAsDouble();
+        Location location = new Location(responseLat, responseLon);
+
+        Hospital hospital = new HospitalImpl(name, vacancies, location);
+
+        model.addAttribute("name", hospital.getName() );
+        model.addAttribute("vacancies", hospital.getVacancies() );
+        model.addAttribute("latitude", hospital.getLocation().getLatitude() );
+        model.addAttribute("longitude", hospital.getLocation().getLongitude());
+
+        return "resultado";
     }
     
     @ExceptionHandler(Exception.class)
@@ -75,5 +68,3 @@ public class HomeController {
         return "error";
     }
 }
-
-
---
+++ src/main/java/br/com/imd/pd/hospital/controllers/HttpUtils.java
@@ -0,0 +1,44 @@
+package br.com.imd.pd.hospital.controllers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+public class HttpUtils {
+    public static String post(String url, Map<String, String> headers, String body) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // Define o método HTTP para POST
+        con.setRequestMethod("POST");
+
+        // Adiciona os headers da requisição
+        if (headers != null) {
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                con.setRequestProperty(entry.getKey(), entry.getValue());
+            }
+        }
+
+        // Habilita o envio do corpo da requisição
+        con.setDoOutput(true);
+
+        // Define o corpo da requisição
+        con.getOutputStream().write(body.getBytes("UTF-8"));
+
+        // Realiza a requisição HTTP e recebe a resposta
+        int responseCode = con.getResponseCode();
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
+    }
+}
